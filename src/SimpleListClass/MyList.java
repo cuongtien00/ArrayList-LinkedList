aec0f63ef1b129a2834950aea3dfb8f581dd1d92
cuongtien00 <cuonghiep000@gmail.com>
2021-09-29 10:44:14 +0700

    add SimpleListClass

---
+++ src/SimpleListClass/MyList.java
@@ -0,0 +1,31 @@
+package SimpleListClass;
+
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements [];
+
+    public MyList() {
+        elements = new Object [DEFAULT_CAPACITY];
+    }
+
+    private void ensureCapa(){
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+    }
+    public E get(int i) {
+        if (i>= size || i <0) {
+            System.out.println("Index out of bounds exception");
+        }
+        return (E)elements[i];
+    }
+}
